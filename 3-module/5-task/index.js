059370efd06c4b363f085d69a0270a9d71395fe9
Oleg Kotov <spartakovec38@gmail.com>
2025-01-16 11:25:02 +0300

    Добавил решение 3-module

--- 3-module/5-task/index.js
+++ 3-module/5-task/index.js
@@ -1,3 +1,10 @@
 function getMinMax(str) {
-  // ваш код...
+  let numbers = str.split(' ')
+  .map((item)=> Number(item))
+  .filter((item) => !isNaN(item));
+
+  let min = Math.min(...numbers);
+  let max = Math.max(...numbers);
+
+  return { min, max};
 }
