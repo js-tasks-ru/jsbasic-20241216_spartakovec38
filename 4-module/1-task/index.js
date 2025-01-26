5aca77b3b4caf197e59dcd9bcf7094692c5f2336
Oleg Kotov <spartakovec38@gmail.com>
2025-01-26 15:41:34 +0300

    Добавил решение 4,5 module

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -1,3 +1,13 @@
 function makeFriendsList(friends) {
-  // ваш код...
+  let names = friends.map(
+    (element) => `${element.firstName} ${element.lastName}`
+  );
+
+  let list = document.createElement("ul");
+
+  for (let name of names) {
+    list.insertAdjacentHTML("beforeEnd", `<li>${name}</li>`);
+  }
+
+  return list;
 }
--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -1,3 +1,35 @@
 function initCarousel() {
-  // ваш код...
+  const carouselInner = document.querySelector(".carousel__inner");
+  const slides = document.querySelectorAll(".carousel__slide");
+  const leftArrow = document.querySelector(".carousel__arrow_left");
+  const rightArrow = document.querySelector(".carousel__arrow_right");
+
+  let currentSlide = 0;
+  const slideWidth = slides[0].offsetWidth;
+
+  function updateArrows() {
+    leftArrow.style.display = currentSlide === 0 ? "none" : "";
+    rightArrow.style.display = currentSlide === slides.length - 1 ? "none" : "";
+  }
+
+  function showNextSlide() {
+    if (currentSlide < slides.length - 1) {
+      currentSlide++;
+      carouselInner.style.transform = `translateX(-${
+        currentSlide * slideWidth}px)`;updateArrows();
+    }
+  }
+
+  function showPrevSlide() {
+    if (currentSlide > 0) {
+      currentSlide--;
+      carouselInner.style.transform = `translateX(-${
+        currentSlide * slideWidth}px)`;updateArrows();
+    }
+  }
+
+  rightArrow.addEventListener("click", showNextSlide);
+  leftArrow.addEventListener("click", showPrevSlide);
+
+  updateArrows();
 }
